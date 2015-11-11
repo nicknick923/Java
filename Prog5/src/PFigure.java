5bd06956d889daab36f44f19d28c0e1968889a68
Nick Sosinski <Nick.j.Sosinski@gmail.com>
2015-11-10 21:04:33 -0600

    Fixed it

--- Prog5/src/PFigure.java
+++ Prog5/src/PFigure.java
@@ -16,8 +16,6 @@ public abstract class PFigure implements Comparable
    protected int priority;       // Can use to determine "winner"
    protected Panel panel;        // Panel the figure lives on
 
-   penis
-   
    public PFigure ( int startX, int startY, int _width, int _height, 
                     int pr, Panel p )
    {
