4da609774af3d29d6987c9b1766a2189063327fe
Nick Sosinski <Nick.j.Sosinski@gmail.com>
2015-11-14 12:01:43 -0600

    Working On Enemies

--- Prog5Work/src/listOfPFigures.java
+++ Prog5Work/src/listOfPFigures.java
@@ -1,43 +1,53 @@
 
-import java.awt.Color;
-import java.awt.Graphics;
 
+import java.awt.*;
 
 /**
 
  @author Nick Sosinski
  @author Jake Ira
  */
-
 import java.awt.*;
+
 public class listOfPFigures
 {
 
    private final int figMax = 10;
    private PFigure[] listOfFigures = new PFigure[figMax];
    private int figCount = 0;
+   private Panel workingPanel;
 
-   public void removeAllFigures(Panel p)
+   public listOfPFigures(Panel p)
+   {
+      workingPanel = p;
+   }
+
+   public void removeAllFigures()
    {
       for (int i = 0; i < figCount; i++)
          listOfFigures[i].hide();
       figCount = 0;
-       
-      Graphics g = p.getGraphics();
-      Color oldColor = g.getColor();
-      g.setColor(p.getBackground());
-      g.fillRect(0, 0, p.getWidth(), p.getHeight());
-      g.setColor(oldColor);
+
+      hideAll();
    }
 
    public void drawAll()
    {
+      hideAll();
       for (int i = 0; i < figCount; i++)
       {
-         listOfFigures[i].hide();
          listOfFigures[i].move();
          listOfFigures[i].draw();
       }
+   }
+
+   private void hideAll()
+   {
+      Graphics g = workingPanel.getGraphics();
+      Color oldColor = g.getColor();
+      g.setColor(workingPanel.getBackground());
+      g.fillRect(0, 0, workingPanel.getWidth(), workingPanel.getHeight());
+      g.setColor(oldColor);
    }
 
    public void addFigure(PFigure inFigure)
