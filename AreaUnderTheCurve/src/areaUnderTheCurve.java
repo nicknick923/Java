396389a7032bcaeb6d51a6fb25759954a25a3067
Nick Sosinski <Nick.j.Sosinski@gmail.com>
2015-11-13 22:53:08 -0600

    new file:   AreaUnderTheCurve/src/areaUnderTheCurve.java Editing AUTC

---
+++ AreaUnderTheCurve/src/areaUnderTheCurve.java
@@ -0,0 +1,13 @@
+
+/**
+
+ @author Nick Sosinski
+ @author Jake Ira
+ */
+public class areaUnderTheCurve
+{
+   public static void main(String args[])
+   {
+      System.out.println("jake sucks a fat dick");
+   }
+}
