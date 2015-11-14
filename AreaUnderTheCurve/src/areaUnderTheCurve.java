048ddfdeb07fefde186ca59605144930f8d50291
jakeira11 <jacob.ira12@gmail.com>
2015-11-13 23:10:07 -0600

    i have the runs

--- AreaUnderTheCurve/src/areaUnderTheCurve.java
+++ AreaUnderTheCurve/src/areaUnderTheCurve.java
@@ -10,13 +10,15 @@ public class areaUnderTheCurve
 {
 
    private static Scanner stdin;
-   
+
    private int startingX;
    private int endingX;
    private int numberOfRectangles = 100000;
 
    public static void main(String args[])
    {
-         stdin = new Scanner(System.in);
-   }  
+      stdin = new Scanner(System.in);
+      for(int i =0; i < 10; i++)
+         System.out.println(stdin.next());
+   }
 }
