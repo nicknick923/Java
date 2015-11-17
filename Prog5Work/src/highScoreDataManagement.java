9c36d66ddf87efb05f5816c973275b85cc96371c
Nick Sosinski <Nick.j.Sosinski@gmail.com>
2015-11-16 21:57:12 -0600

    Added the HSDM 	new file:   Prog5Work/src/highScoreDataManagement.java

---
+++ Prog5Work/src/highScoreDataManagement.java
@@ -0,0 +1,18 @@
+
+/**
+
+ @author Nick Sosinski
+ @author Jake Ira
+ */
+import java.util.Scanner;
+
+public class highScoreDataManagement
+{
+
+   public highScoreDataManagement()
+   {
+      if (Prog5GUI.isHSFileOpen())
+         Prog5GUI.closeHS();
+
+   }
+}
