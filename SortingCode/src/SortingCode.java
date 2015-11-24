d24dcc969eedde9f614035d8285efec2e7ca07f5
Nick Sosinski <Nick.j.Sosinski@gmail.com>
2015-11-23 20:52:35 -0600

    Added some sorting stuff 	modified:   Prog3/iraj_p3Test.docx 	new file:   SortingCode/build.xml 	new file:   SortingCode/manifest.mf 	new file:   SortingCode/nbproject/build-impl.xml 	new file:   SortingCode/nbproject/genfiles.properties 	new file:   SortingCode/nbproject/project.properties 	new file:   SortingCode/nbproject/project.xml 	new file:   SortingCode/src/SortingCode.java 	new file:   SortingCode/src/Sorts.java

---
+++ SortingCode/src/SortingCode.java
@@ -0,0 +1,15 @@
+
+/**
+
+ @author Nick Sosinski
+ @author Jake Ira
+ */
+public class SortingCode
+{
+
+   public static void main(String args[])
+   {
+      Sorts mySort = new Sorts();
+      mySort.run();
+   }
+}
---
+++ SortingCode/src/Sorts.java
@@ -0,0 +1,58 @@
+
+import jdk.nashorn.internal.objects.AccessorPropertyDescriptor;
+
+/**
+
+ @author Nick Sosinski
+ @author Jake Ira
+ */
+public class Sorts
+{
+
+   private int ary[];
+
+   public Sorts()
+   {
+      resetArray();
+   }
+
+   public void run()
+   {
+      System.out.println("Unsorted:");
+      printArray(ary);
+      bubbleSort();
+      
+   }
+
+   private void resetArray()
+   {
+      ary = new int[]
+      {
+         12, 6, 856, 43, -34, 8, 754, -643
+      };
+   }
+
+   private void bubbleSort()
+   {
+      System.out.println("Bubble Sort:");
+      for (int i = 0; i <= ary.length; i++)
+         for (int j = ary.length - 1; j >= i + 1; j--)
+            if (ary[j] < ary[j - 1])
+               swap(ary, j, j - 1);
+      printArray(ary);
+      resetArray();
+   }
+
+   private void printArray(int[] array)
+   {
+      for (int i = 0; i < array.length; i++)
+         System.out.println(array[i]);
+   }
+
+   private void swap(int[] array, int index1, int index2)
+   {
+      int temp = array[index2];
+      array[index2] = array[index1];
+      array[index1] = temp;
+   }
+}
