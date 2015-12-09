ac7e3b1f95189e07295067452233b82fd3838def
Nick Sosinski <Nick.j.Sosinski@gmail.com>
2015-12-08 20:32:24 -0600

    some stuff 	modified:   testTest/src/testTest.java

--- testTest/src/testTest.java
+++ testTest/src/testTest.java
@@ -1,4 +1,6 @@
 
+import com.sun.prism.shader.AlphaTextureDifference_ImagePattern_Loader;
+
 /**
 
  @author Nick Sosinski
@@ -13,13 +15,23 @@ public class testTest
       Node p;
 
       list = new Node(1, new Node(2, new Node(3, new Node(4, null))));
-      java.util.Scanner stdin = new java.util.Scanner(System.in);
-      while (stdin.hasNext())
-      {
-         int inInt = stdin.nextInt();
-         System.out.println(inInt % 11);
-      }
+      if (list != null && list.next != null)
+         if (list.next.next == null)
+         {
+            Node tempNode = list.next;
+            list.next = list;
+            list = tempNode;
+         }
+         else
+         {
+            p = list;
+            while (p.next.next != null)
+               p = p.next;
+            Node tempNode = list.next;
+            list.next = list;
+            list = tempNode;
 
+         }
       System.out.println("hi");
    }
 }
